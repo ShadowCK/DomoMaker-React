62d85f08163cac34355c1d7c212e6b488196705b
ShadowCK <540712348@qq.com>
2023-11-12 07:17:17 -0500

    DomoMakerD Step8 Finished

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,20 @@
 const path = require('path');
 
 module.exports = {
-    entry: './client/client.js',
-    mode: 'production',
-    watchOptions: {
-        aggregateTimeout: 200,
+  entry: './client/client.js',
+  module: {
+    rules: [{ test: /\.(js|jsx)$/ }],
+    exclude: /node_modules/,
+    use: {
+      loader: 'babel-loader',
     },
-    output: {
-        path: path.resolve(__dirname, 'hosted'),
-        filename: 'bundle.js',
-    },
+  },
+  mode: 'production',
+  watchOptions: {
+    aggregateTimeout: 200,
+  },
+  output: {
+    path: path.resolve(__dirname, 'hosted'),
+    filename: 'bundle.js',
+  },
 };
